f945a30e63196cf0172e0a3d5acc1ab171ef0022
sammyteahan <sammyteahan@gmail.com>
2023-07-31 09:32:33 -0600

    Tweaks to mason and git signs

--- init.lua
+++ init.lua
@@ -122,7 +122,14 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Setup mason so it can manage external tooling
-require('mason').setup()
+-- the python specific config fixed failures I was
+-- encountering when trying to upgrade lsp within
+-- Mason
+require('mason').setup {
+  pip = {
+    upgrade_pip = true,
+  }
+}
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
