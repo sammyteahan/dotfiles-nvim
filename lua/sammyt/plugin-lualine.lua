1cd2ef2d4cadecf378b58d3dd49d1de06fd93031
sammyteahan <sammyteahan@gmail.com>
2023-11-14 09:43:38 -0700

    update lualine theme to match the rest

--- lua/sammyt/plugin-lualine.lua
+++ lua/sammyt/plugin-lualine.lua
@@ -3,7 +3,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'nord',
+    theme = 'tokyonight',
     component_separators = '|',
     section_separators = '',
   },
